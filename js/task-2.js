5fe83602cc493d43143f196d4a964585f3d0ba9f
OlegUsik21 <149603285+OlegUsik21@users.noreply.github.com>
2024-04-06 19:22:40 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,26 @@
+class Storage {
+  #items;
+
+  constructor(initialItems) {
+    this.#items = initialItems;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(deleteItem) {
+    this.#items = this.#items.filter(item => item !== deleteItem);
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
